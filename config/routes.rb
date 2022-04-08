fbe0be62e42b55b2883c9dab1d703829ff184468
Frannyontech <fran.galazojeda@gmail.com>
2022-04-07 22:22:38 -0400

    scaffold_controller users

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   resources :tags
   resources :cafes
+  resources :users, except: [:create, :destroy]
   devise_for :users
  
   get 'dashboard', to: 'home#index'
